3c8e9eabf26897aad79b2f6a777c774e335dcf62
Mert Bölükbaşı <160646761+MertBlk@users.noreply.github.com>
2024-05-28 23:53:14 +0300

    Add files via upload

--- m23210202036.py
+++ m23210202036.py
@@ -2,7 +2,7 @@ def kontor_al(kontor_miktari):
     while True:
         try:
             alinan_kontor = int(input("Kontör miktarı giriniz: "))
-            if alinan_kontor < 0 or alinan_kontor > 120:
+            if alinan_kontor < 0 or alinan_kontor >= 120:
                 print("0'dan küçük 120'den büyük değer girilmez.")
             else:
                 kontor_miktari += alinan_kontor
@@ -13,26 +13,29 @@ def kontor_al(kontor_miktari):
     return kontor_miktari
 
 
-def gorusme_yap(kontor_miktari, gorusme_suresi):
+def gorusme_yap(kontor_miktari, gorusme_suresi, toplam_gorusme_suresi):
     if kontor_miktari < gorusme_suresi * 0.12:
         print("Telefonda yeterli kontör olmadığı için görüşme yapamazsınız.")
-        return kontor_miktari
+        return kontor_miktari, toplam_gorusme_suresi  # Toplam görüşme süresini de döndürüyoruz
     else:
         kontor_miktari -= gorusme_suresi * 0.12
+        toplam_gorusme_suresi += gorusme_suresi  # Toplam görüşme süresine ekleme yapılıyor
         print(f"{gorusme_suresi} dakika görüşme yaptınız.")
-        return kontor_miktari
+        print(f"Toplam {toplam_gorusme_suresi} dakika görüşme yaptınız.")  # Toplam görüşme süresi yazdırılıyor
+        
+        return kontor_miktari, toplam_gorusme_suresi  # Güncellenmiş kontör ve toplam görüşme süresini döndürüyoruz
     
 
 def kontor_bilgisi(kontor_miktari):
     print("Telefonda {:.1f} liralık kontör vardır.".format(kontor_miktari))
 
 
-def gorusme_bilgisi(gorusme_suresi):
-    print(f"{gorusme_suresi} dakika görüşme yaptınız.")
+def gorusme_bilgisi(toplam_gorusme_suresi):
+    print(f"Toplam {toplam_gorusme_suresi} dakika görüşme yaptınız.")
 
 
 kontor_miktari = 0.0
-son_gorusme_suresi = 0
+toplam_gorusme_suresi = 0
 
 while True:
     print("\n--- Telefon Uygulaması ---")
@@ -47,8 +50,7 @@ while True:
     elif secim == "2":
         try:
             gorusme_suresi = int(input("Kaç dakika görüşme yapacaksınız: "))
-            kontor_miktari = gorusme_yap(kontor_miktari, gorusme_suresi)
-            son_gorusme_suresi = gorusme_suresi
+            kontor_miktari, toplam_gorusme_suresi = gorusme_yap(kontor_miktari, gorusme_suresi, toplam_gorusme_suresi)
         except ValueError:
             print("Geçersiz değer girildi, lütfen tekrar deneyin.")
     elif secim == "3":
@@ -61,7 +63,7 @@ while True:
             if menu_secim == "1":
                 kontor_bilgisi(kontor_miktari)
             elif menu_secim == "2":
-                gorusme_bilgisi(son_gorusme_suresi)
+                gorusme_bilgisi(toplam_gorusme_suresi)
             elif menu_secim == "9":
                 break
             else:
